25899e475277696356743b87132a21253bae5b7c
bcwahoo <bcwahoo@gmail.com>
2019-11-12 00:08:41 -0500

    Revisions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,98 +1,110 @@
-// Artist Button
+$(document).ready(function() {
+  // Artist Button
+  var artists = [
+    "Biggie",
+    "Wu-Tang",
+    "Timbaland",
+    "Meek Miilz",
+    "Beanie Sigel",
+    "Papoose",
+    "Rick Ross",
+    "Jay-Z",
+    "DMX"
+  ];
 
-var artists = [
-  "Biggie",
-  "Wu-Tang",
-  "Timbaland",
-  "Meek Miilz",
-  "Beanie Sigel",
-  "Papoose",
-  "Rick Ross",
-  "Jay-Z",
-  "DMX"
-];
+  function giveMeButtons() {
+    $("#rap-buttons").empty();
 
-function giveMeButtons() {
-  $("#button-display").empty();
+    for (var loop = 0; loop < artists.length; loop++) {
+      var topBtn = $('<button class="btn btn-success">');
 
-  for (var loop = 0; loop < artists.length; loop++) {
-    
-    var topBtn = $("<button>");
-
-    topBtn.addClass("btn btn-primary btn-sm");
-
-    topBtn.attr("data-name", artists[loop]);
-
-    topBtn.text(artists[loop]);
-
-    $("#button-display").append(topBtn);
+      topBtn.addClass("rap-button");
+      topBtn.attr("data-name", artists[loop]);
+      topBtn.text(artists[loop]);
+      $("#rap-buttons").append(topBtn);
+    }
   }
-}
 
-$("#add-artist").on("click", function(event) {
-  event.preventDefault();
+  // Adding click event listen listener to all buttons
+  $(document).on("click", ".rap-button", function() {
+    $("#a-display").empty();
+    $(".rap-button").removeClass("active");
+    $(this).addClass("active");
 
-  var artist = $("#artist-input").val().trim();
+    var artGif = $(this).attr("data-name");
+    console.log(artGif);
 
-  artists.push(artist);
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      artGif +
+      "&api_key=jIvi6w2Fe9qAQkZtycr302I612DQsfqM&limit=10";
 
-  giveMeButtons();
-});
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+        var results = response.data;
 
-giveMeButtons();
+        if (results.length > 0) {
+          var gifRow = $('<div class="row">');
 
-//Freeze Image
-function setImageStatus() {
-  var state = $(this).attr("data-state");
-  if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-  }
-  else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-  }
-}
-
- // Adding click event listen listener to all buttons
- $("#button-display").on("click", function() {
-  // Grabbing and storing the data-animal property value from the button
-  var artGif = $(this).attr("data-name");
-
-  // Constructing a queryURL using the animal name
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    artGif + "&api_key=jIvi6w2Fe9qAQkZtycr302I612DQsfqM&limit=10";
-
-  // Performing an AJAX request with the queryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  // After data comes back from the request
-  .then(function(response) {
-    console.log(queryURL);
-    console.log(response);
-    var results = response.data;
-    if (results.length > 0) {
-        var $gifRow = $('<div class="row">');
-        for (var loop = 0; loop < results.length; loop++) {
+          for (var loop = 0; loop < results.length; loop++) {
+            var gifCard = $('<div class="card col-sm-5">');
             var rating = results[loop].rating;
             rating = rating.toUpperCase();
+
             var imageAnimateName = results[loop].images.fixed_height.url;
             var imageStillName = results[loop].images.fixed_height_still.url;
-            var $gifSpan = $('<span class="col-sm-4">');
-            var $p = $('<p style="padding-top: 40px">').text("Rating: " + rating);
-            var rapperImage = $("<img>");
-            rapperImage.addClass("gif");
+            var p = $('<h5 class="card-title mx-auto">').text(
+              "Rating: " + rating
+            );
+            var rapperImage = $('<img class="card-img-top">');
+
             rapperImage.attr("data-state", "still");
             rapperImage.attr("src", imageStillName);
             rapperImage.attr("data-still", imageStillName);
             rapperImage.attr("data-animate", imageAnimateName);
-            $gifSpan.append($p);
-            $gifSpan.append(rapperImage);
-            $gifRow.append($gifSpan);}
+            rapperImage.addClass("rapper-pic");
 
-        $("#gif-display").prepend($gifRow);
+            gifCard.append(p);
+            gifCard.append(rapperImage);
+            gifRow.append(gifCard);
+          }
+
+          $("#a-display").append(gifRow);
         }
       });
-    })
+  });
+
+  //Freeze Image
+  $(document).on("click", ".rapper-pic", function() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+  //Create Buttons
+  $("#add-artist").on("click", function(event) {
+    event.preventDefault();
+    var newArtist = $("#artist-input").val().trim();
+
+    if (newArtist.length > 2) {
+      artists.push(newArtist);
+    }
+
+    giveMeButtons(artists, "rap-button", "#rap-buttons");
+  });
+
+  giveMeButtons(artists, "rap-button", "#rap-buttons");
+});
